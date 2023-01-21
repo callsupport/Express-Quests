218038ef5f649ea32565632656a308d36babbe9b
Nassima Ayadi <gsmicrodev@gmail.com>
2023-01-21 02:42:11 +0100

    added put route /api/users/:id

--- app.js
+++ app.js
@@ -16,11 +16,13 @@ const movieHandlers = require("./movieHandlers");
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.post("/api/movies", movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
 
 const usersHandlers = require("./usersHandlers");
 app.get("/api/users", usersHandlers.getUsers);
 app.get("/api/users/:id", usersHandlers.getUsersById);
 app.post("/api/users", usersHandlers.postUsers);
+app.put("/api/users/:id", usersHandlers.updateUsers);
 
 app.listen(port, (err) => {
   if (err) {
