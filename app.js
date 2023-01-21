b502fa51bbcf46ff2ab756b33d6fa8a2b84cd29e
Nassima Ayadi <gsmicrodev@gmail.com>
2023-01-21 03:03:15 +0100

    added delete route /api/users/:id

--- app.js
+++ app.js
@@ -17,12 +17,14 @@ app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.post("/api/movies", movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
 const usersHandlers = require("./usersHandlers");
 app.get("/api/users", usersHandlers.getUsers);
 app.get("/api/users/:id", usersHandlers.getUsersById);
 app.post("/api/users", usersHandlers.postUsers);
 app.put("/api/users/:id", usersHandlers.updateUsers);
+app.delete("/api/users/:id", usersHandlers.deleteUsers);
 
 app.listen(port, (err) => {
   if (err) {
--- movieHandlers.js
+++ movieHandlers.js
@@ -65,8 +65,6 @@ const postMovie = (req, res) => {
     });
 };
 
-// in movieHandlers.js
-
 const updateMovie = (req, res) => {
   const id = parseInt(req.params.id);
   const { title, director, year, color, duration } = req.body;
@@ -88,10 +86,28 @@ const updateMovie = (req, res) => {
       res.status(500).send("Error editing the movie");
     });
 };
+const deleteMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("delete from movies where id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error deleting the movie");
+    });
+};
 
 module.exports = {
   getMovies,
   getMovieById,
   postMovie,
   updateMovie,
+  deleteMovie,
 };
