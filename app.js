de0d97a3b2cc78387cc725db66400615e25bb422
Nassima Ayadi <gsmicrodev@gmail.com>
2023-01-24 11:56:17 +0100

    added validators.js

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require("express");
 const app = express();
 
 const port = process.env.APP_PORT ?? 8000;
+
 app.use(express.json());
+
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
@@ -15,16 +17,20 @@ const movieHandlers = require("./movieHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
 const usersHandlers = require("./usersHandlers");
 app.get("/api/users", usersHandlers.getUsers);
 app.get("/api/users/:id", usersHandlers.getUsersById);
-app.post("/api/users", usersHandlers.postUsers);
-app.put("/api/users/:id", usersHandlers.updateUsers);
 app.delete("/api/users/:id", usersHandlers.deleteUsers);
+
+const { validateMovie } = require("./validators.js");
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
+
+const { validateUser } = require("./validators.js");
+app.post("/api/users", validateUser, usersHandlers.postUsers);
+app.put("/api/users/:id", validateUser, usersHandlers.updateUsers);
 
 app.listen(port, (err) => {
   if (err) {
